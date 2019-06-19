c8723bbcf6f1042ef0c3a289ca8aaa0de4237439
katanotakashi <katanotakashi99@gmail.com>
2019-06-20 05:33:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/aisatu/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.aisatu
+
+import android.app.TimePickerDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View)  {
+        showTimePickerDialog()
+    }
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                if ( hour < 2) {
+                    textView.text = "こんばんは"
+                } else if ( hour < 10){
+                    textView.text = "おはよう"
+                } else if ( hour < 18){
+                    textView.text = "こんにちは"
+                } else {
+                    textView.text = "こんばんは"
+                }
+            },
+            13, 0, true)
+        timePickerDialog.show()
+    }
+
+}
